3a908e546f53edfc5029066fe5a455dc87d9ec30
Jason <Jason.Simms@adesso.de>
2024-03-27 16:50:26 +0100

    added SSR vercel

---
+++ src/pages/api/test.ts
@@ -0,0 +1,12 @@
+export async function GET() {
+
+
+  return new Response(
+    JSON.stringify({msg: "Hello World"}), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
+}
